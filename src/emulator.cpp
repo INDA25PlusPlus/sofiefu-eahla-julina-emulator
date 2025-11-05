7b3ad5b16199ea8b9c083a8563ea26be91eb23ba
JulinaSavela <julina.savela@outlook.com>
2025-11-05 11:31:45 +0100

    feat: emulator struct

---
+++ src/emulator.cpp
@@ -0,0 +1,37 @@
+
+#include <vector>
+using namespace std;
+
+struct Emulator
+{   
+    // register
+    uint8_t A, B, C, D, E, H, L;
+
+    // stack pointer och program counter
+    uint16_t SP;
+    uint16_t PC;
+
+    // flags
+    // | Flag | Name            |
+    // | ---- | --------------- |
+    // | Z    | Zero            |
+    // | S    | Sign            |
+    // | P    | Parity          |
+    // | CY   | Carry           |
+    // | AC   | Auxiliary Carry |
+
+    bool Z, S, P, CY, AC;
+
+    // minne, intel 8080 har 64kB minne
+    vector<uint8_t> memory;
+
+    Emulator() : memory(65000) {
+
+        A=B=C=D=E=H=L = 0;
+        SP=PC = 0;
+        Z=S=P=CY=AC = false;
+    }
+
+
+};
+
